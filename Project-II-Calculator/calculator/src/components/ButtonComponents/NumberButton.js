a7a18cc448aaa5a81ce90751c5213be45b3e10d2
Anna W <anski.anna@gmail.com>
2019-05-28 15:31:25 +0100

    added components to the calculator project. Got the numbers displayed

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const numbers = [1,2,3,4,5,6,7,8,9,0];
+
+function NumbersButton (props){
+    return (
+        <div className="numbers">
+            {numbers.map(number => <button key={number} className={props.buttonStyle}>{number}</button>)}
+        </div>
+    )
+}
+
+
+export default NumbersButton;
